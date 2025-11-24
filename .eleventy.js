7d11a7c768e709c295ab4a80501ff4b416f511d8
Marc Jentsch <marc.jentsch@hsh-berlin.com>
2025-11-24 08:35:30 +0100

    ...

--- .eleventy.js
+++ .eleventy.js
@@ -1,14 +1,14 @@
 module.exports = function (eleventyConfig) {
-    eleventyConfig.addCollection("artworks", function (collection) {
-        return collection.getFilteredByGlob("src/artworks/*.md");
-    });
-    eleventyConfig.addPassthroughCopy("src/images");
-    eleventyConfig.addPassthroughCopy("src/assets");
-    return {
-        dir: {
-            input: "src",
-            output: "dist"
-        }
-    };
-
+  eleventyConfig.addCollection("artworks", function (collection) {
+    return collection.getFilteredByGlob("src/artworks/*.md");
+  });
+  eleventyConfig.addPassthroughCopy("src/images");
+  eleventyConfig.addPassthroughCopy("src/assets");
+  return {
+    dir: {
+      input: "src",
+      output: "dist",
+    },
+    pathPrefix: "/marc-gerrit-artist/",
+  };
 };
